10a73e88c0dfd876c2e6f1efebd2108c1b2b196b
Endra Dwi <endradwijamludin@gmail.com>
2025-05-07 17:25:03 +0700

    feat: fetching register

--- routers/main.go
+++ routers/main.go
@@ -1,8 +1,18 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func Routers(router *gin.Engine) {
+	router.Use(cors.New(cors.Config{
+		// AllowAllOrigins: true,
+		AllowOrigins:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
+		AllowCredentials: true,
+	}))
 	AuthRouters(router.Group("/auth"))
 	DoctorRouter(router.Group("/doctor"))
 	UserRouter(router.Group("/users"))
