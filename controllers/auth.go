7764654916c63158f0f6cb135a9f39aebc66d382
Endra Dwi <endradwijamludin@gmail.com>
2025-06-16 13:37:34 +0700

    chore: cek test

--- controllers/auth.go
+++ controllers/auth.go
@@ -100,7 +100,7 @@ func AuthLogin(ctx *gin.Context) {
 		"role_id": foundUser.Role_Id,
 	}
 
-	ctx.SetCookie("token", token, 3600, "/", "", false, false)
+	ctx.SetCookie("token", token, 3600, "/", "", true, true)
 
 	// })
 
